e07b1a33f2ed29baeac1d9c8e063db3019720dcb
byeongkookkang <119069089+byeongkookkang@users.noreply.github.com>
2025-02-12 21:14:51 +0900

    chore: Nav바 hover시 파란색으로 보여주기

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,16 @@
 import '../App.scss';
 import { useNavigate } from 'react-router';
+import cheston from '../img/cheston.png';
 
 export default function Navigation() {
   const navigate = useNavigate();
   return (
     <div className="nav-background">
       <nav className="nav-container">
-        <div className="nav-item">체스턴</div>
-        <div className="nav-item" onClick={() => navigate('/')}>홈</div>
+        <div className='nav-item' onClick={() => navigate('/')}>
+          <img src={cheston}  width='60px'/>
+          <span>Cheston</span>
+        </div>
         <div style={{ flexGrow: 1 }}></div>
         <div 
           className="nav-item" 
@@ -15,7 +18,9 @@ export default function Navigation() {
         >
           로그인
         </div>
-        <div className="nav-item" onClick={() => { window.location.href = "https://forms.gle/p7yHK9VgE1588KXT9"; }}>회원가입</div>
+        <div className="nav-item" onClick={() => { window.location.href = "https://forms.gle/p7yHK9VgE1588KXT9"; }}>
+          <button className='btn'>회원가입</button>
+        </div>
       </nav>
     </div>
   );
