75910d9970b925030d5c69382b3dc64299bb1ba8
byeongkookkang <119069089+byeongkookkang@users.noreply.github.com>
2025-02-15 00:47:29 +0900

    refactor: 상세페이지 SCSS 스타일링 전면 개편

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { formatDate, formatPrice } from "../functions/functions";
 import { useSelector } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
-import "./Detail.scss"; 
+import "./Detail.css";
 
 function Detail() {
   const { id } = useParams();
@@ -29,23 +29,26 @@ function Detail() {
   if (error) return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
 
   const {
-    jwn_lst,
-    case_num,
-    property_num,
-    property_type,
-    appraisal_val,
-    minimum_sale_prc,
-    bidding_method,
-    sale_date,
-    prprty_notes,
-    location,
-    responsible_total,
-    case_filing_date,
-    auction_strt_date,
-    dmnd_date,
-    claim_amt,
-    due_dates,
-    case_details,
+    jwn_lst,            // 법원정보
+    case_num,           // 사건번호
+    property_num,       // 물건번호
+    property_type,      // 물건종류
+    appraisal_val,      // 감정가
+    minimum_sale_prc,   // 최저매각가격
+    bidding_method,     // 매각방법
+    sale_date,          // 매각기일
+    prprty_notes,       // 비고
+
+    case_filing_date,   // 소송 접수일
+    auction_strt_date,  // 경매 시작일
+    dmnd_date,          // 배당요구종기일
+    claim_amt,          // 청구금액
+
+    // 그 외
+    location,           // 건물위치
+    responsible_total,  // 경매계
+    due_dates,          // 매각기일 정보 리스트
+    case_details,       // 세부 정보 리스트
   } = item;
 
   return (
@@ -54,70 +57,66 @@ function Detail() {
       <div>
         <h4 className="section-title">기본정보</h4>
         <hr />
+        {/* 3×3 표 (법원정보 ~ 비고) */}
         <table className="detail-table">
           <tbody>
             <tr>
-              <th style={{ width: "30%" }}>법원정보</th>
+              <th>법원정보</th>
               <td>{jwn_lst}</td>
-            </tr>
-            <tr>
               <th>사건번호</th>
               <td>{case_num}</td>
-            </tr>
-            <tr>
               <th>물건번호</th>
               <td>{property_num}</td>
             </tr>
             <tr>
               <th>물건종류</th>
               <td>{property_type}</td>
-            </tr>
-            <tr>
               <th>감정가</th>
               <td>{appraisal_val ? formatPrice(appraisal_val) : ""}</td>
-            </tr>
-            <tr>
               <th>최저매각가격</th>
               <td>{minimum_sale_prc ? formatPrice(minimum_sale_prc) : ""}</td>
             </tr>
             <tr>
               <th>매각방법</th>
               <td>{bidding_method}</td>
-            </tr>
-            <tr>
               <th>매각기일</th>
               <td>{sale_date ? formatDate(sale_date) : ""}</td>
-            </tr>
-            <tr>
               <th>비고</th>
               <td>{prprty_notes}</td>
             </tr>
-            <tr>
-              <th>건물위치</th>
-              <td>{location}</td>
-            </tr>
-            <tr>
-              <th>경매계</th>
-              <td>{responsible_total}</td>
-            </tr>
+          </tbody>
+        </table>
+
+        {/* 2×2 표 (소송접수일 ~ 청구금액) */}
+        <table className="detail-table">
+          <tbody>
             <tr>
               <th>소송 접수일</th>
               <td>{case_filing_date ? formatDate(case_filing_date) : ""}</td>
-            </tr>
-            <tr>
               <th>경매 시작일</th>
               <td>{auction_strt_date ? formatDate(auction_strt_date) : ""}</td>
             </tr>
             <tr>
               <th>배당요구종기일</th>
               <td>{dmnd_date ? formatDate(dmnd_date) : ""}</td>
-            </tr>
-            <tr>
               <th>청구금액</th>
               <td>{claim_amt ? formatPrice(claim_amt) : ""}</td>
             </tr>
           </tbody>
         </table>
+        <table className="detail-table">
+          <tbody>
+            <tr>
+              <th>건물위치</th>
+              <td>{location}</td>
+            </tr>
+            <tr>
+              <th style={{width:'100px'}}>경매계</th>
+              <td>{responsible_total}</td>
+            </tr>
+          </tbody>
+        </table>
+
       </div>
 
       {/* 경매정보 섹션 */}
