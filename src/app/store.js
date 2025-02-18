75378e7be0a6036fdbbcc7f4d8135083b4158e91
byeongkookkang <119069089+byeongkookkang@users.noreply.github.com>
2025-02-18 14:22:27 +0900

    chore: axios uraxios 요청 URL에 '/api' 경로 추가

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const urlSlice = createSlice({
   name: 'url',
-  initialState:"http://127.0.0.1:8000/api",
+  initialState:"http://127.0.0.1:8000/api/",
   reducers: {
   }
 })
