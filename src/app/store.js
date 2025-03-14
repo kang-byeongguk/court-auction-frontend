213c42762cac74d9840a954bdf57d76a791fa45b
byeongkookkang <byeongguk1998@gmail.com>
2025-03-14 20:55:19 +0900

    fix: Vite 및 store.js에서 로컬용 작업용 url 수정

--- src/app/store.js
+++ src/app/store.js
@@ -4,9 +4,9 @@ import { createSlice } from '@reduxjs/toolkit'
 export const urlSlice = createSlice({
   name: 'url',
   //배포묭 IPv4 주소
-  initialState:"https://54.180.66.23/api/",
+  // initialState:"https://54.180.66.23/api/",
   //로컬용 url
-  // initialState:"http://127.0.0.1:8000/api/",
+  initialState:"http://127.0.0.1:8000/",
   reducers: {
   }
 })
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/static/dist/',
+  //local 개발용 base
+  base:'/',
+  //배포시 base
+  // base:'/static/dist/',
   plugins: [react()],
 })
